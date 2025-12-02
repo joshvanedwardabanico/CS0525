1b273dd71bb32756e62efec5af7f9603e0d33587
joshvanedwardabanico <150789446+joshvanedwardabanico@users.noreply.github.com>
2025-12-02 14:41:39 +0100

    Create media.c

---
+++ UNIT_1/S2/L2/media.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    float a; //primo numero float
+    float b; //secondo numero float
+    float media; //la media tra i due numeri float
+
+    printf("Inserisci il primo numero: ");
+    scanf("%f",&a);
+
+    printf("Inserisci il secondo numero: ");
+    scanf("%f",&b);
+
+    media = (a+b)/2;
+
+    printf("La media aritmetica è: %f", media);
+
+    return 0;
+}
