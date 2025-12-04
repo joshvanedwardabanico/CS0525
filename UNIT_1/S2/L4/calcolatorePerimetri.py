36c534cb4daafe99c368b5a7fe0d3ec810956a78
joshvanedwardabanico <150789446+joshvanedwardabanico@users.noreply.github.com>
2025-12-04 15:08:32 +0100

    Update calcolatorePerimetri.py

--- UNIT_1/S2/L4/calcolatorePerimetri.py
+++ UNIT_1/S2/L4/calcolatorePerimetri.py
@@ -15,7 +15,7 @@ while a != 4: #ciclo per continuare a chiedere i perimetri finché non preme '4'
     elif a == 3:
             baseRettangolo = float(input("Inserisci la lunghezza della base del rettangolo: "))
             altezzaRettangolo = float(input("Inserisci l'altezza della base del rettangolo: "))
-            perimetroRettangolo = 2 * baseRettangolo + 2 * altezzaRettangolo #calcolo del perimetro del rettangolo
+            perimetroRettangolo = (2 * baseRettangolo) + (2 * altezzaRettangolo) #calcolo del perimetro del rettangolo
             print(f"\nIl perimetro del rettangolo di base {baseRettangolo} e di altezza {altezzaRettangolo} è {perimetroRettangolo}")
     elif a == 4: #uscita dal programma
             print("\nAlla prossima!")
